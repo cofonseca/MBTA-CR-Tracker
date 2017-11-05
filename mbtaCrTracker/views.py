ed780ef085efb58564ba5c0c6cf7f1f890df612a
Corey Fonseca <cfonseca@blueport.com>
2017-11-05 15:16:35 -0500

    Messing with views. Added Jinja2 templating to index.

--- mbtaCrTracker/views.py
+++ mbtaCrTracker/views.py
@@ -1,11 +1,18 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from mbtaCrTracker import app
+#import sys
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    lines = [['Fairmount','Fairmount'],['Fitchburg','Fitchburg'],['Worcester','Framingham'],['Franklin','Franklin'],['Greenbush','Greenbush'],['Kingston','Kingston'],['Middleborough','Lakevile'],['Middleborough','Middleborough'],['Needham','Needham'],['Providence','Providence'],['Providence','Stoughton'],['Worcester','Worcester']]
+    return render_template('index.html', lines=lines)
 
-@app.route('/train/locate', methods=['GET'])
+@app.route('/map', methods=['GET'])
 def map():
-    return render_template('locate.html')
+    line = request.args.get('line')
+    direction = request.args.get('direction')
+    #print(line, file=sys.stdout)
+    #print(direction, file=sys.stdout)
+    #coords = .\mbtaCrTracker\getTrainInfo.py line direction
+    return render_template('map.html')
