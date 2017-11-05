fd1cb8bc30caed7ab1f3650b0c492c2a46a95c8b
Corey Fonseca <cfonseca@blueport.com>
2017-11-05 12:03:16 -0500

    Messing with Routing.

--- mbtaCrTracker/views.py
+++ mbtaCrTracker/views.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from mbtaCrTracker import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/train/locate', methods=['GET'])
+def map():
+    return render_template('locate.html')
