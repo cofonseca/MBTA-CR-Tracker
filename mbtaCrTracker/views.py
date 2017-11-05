ff4997882649d6b7df8e707acdab76480e1d93c1
Corey Fonseca <cfonseca@blueport.com>
2017-11-04 21:19:36 -0400

    Initial Commit

---
+++ mbtaCrTracker/views.py
@@ -0,0 +1,6 @@
+from mbtaCrTracker import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
---
+++ run.py
@@ -0,0 +1,3 @@
+from mbtaCrTracker import app
+from mbtaCrTracker import views
+app.run(debug=True)
