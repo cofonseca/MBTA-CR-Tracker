04a78e4b31dbdde42eb52dae8de28a2d8688c491
Corey Fonseca <cfonseca@blueport.com>
2017-11-05 17:07:09 -0500

    Moved and renamed this file. Added wrapper function.

---
+++ getTrainInfo.py
@@ -0,0 +1,27 @@
+import requests
+import sys
+
+def getTrainInfo(line, direction):
+
+    def getRouteInfo(line, direction):
+        # Gather Info and Make API Call
+        #route = getRoute()
+        #direction = getDirection()
+        url = 'http://realtime.mbta.com/developer/api/v2/vehiclesbyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=CR-'+line+'&format=json'
+        response = (requests.get(url)).json()
+        # Return JSON Object
+        for trip in response['direction']:
+            if trip['direction_name'] == direction:
+                return trip
+
+    def getVehicleCoordinates():
+        latitude = routeInfo['trip'][0]['vehicle']['vehicle_lat']
+        longitude = routeInfo['trip'][0]['vehicle']['vehicle_lon']
+        return latitude,longitude
+
+    routeInfo = getRouteInfo(line, direction)
+    if routeInfo:
+        coordinates = getVehicleCoordinates()
+        return coordinates
+    else:
+        print('No ' + direction + ' trains are currently running on the ' + line + ' line.')
